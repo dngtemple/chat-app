f8c5a5a2a5c7a523c92a3e56171b48a911e3e038
Clinton Templeton <donnahaytempleton1230@gmail.com>
2024-12-01 17:28:52 +0000

    deployment of my chat application

--- backend/server.js
+++ backend/server.js
@@ -2,11 +2,18 @@ import express from "express"
 // const app = express();
 
 import cors from "cors"
+import path from "path"
 
 
 import dotenv from "dotenv"
 dotenv.config()
 const PORT = process.env.PORT || 8001
+
+
+const __dirname = path.resolve()
+
+
+
 
 import cookieParser from "cookie-parser";
 
@@ -30,6 +37,11 @@ app.use(cookieParser())
 app.use("/api/auth",authRoutes);
 app.use("/api/messages",messageRoutes)
 app.use("/api/users",userRoutes)
+
+app.use(express.static(path.join(__dirname,"/frontend/dist")))
+app.get("*",function(req,res){
+  res.sendFile(path.join(__dirname,"frontend","dist","index.html"))
+})
 
 server.listen(PORT,function(){
     connectToMongoDB();
