80f329b354c2a1c66cfc2eb85be67db9f36c9aa5
Clinton Templeton <donnahaytempleton1230@gmail.com>
2024-12-01 18:27:20 +0000

    url changes for render to deploy

--- frontend/src/context/socketContext.jsx
+++ frontend/src/context/socketContext.jsx
@@ -20,7 +20,7 @@ export const SocketContextProvider= ({children})=>{
 
     useEffect(function(){
         if(authUser){
-            const socket=io("http://localhost:8000",{
+            const socket=io("https://chat-app-cfsj.onrender.com",{
                 query:{
                     userId:authUser.id
                 }
